808827b1099a778c98a33fd5755b880cb9fdd3d9
alex gnavi <alex.gnavi@gmail.com>
2022-11-29 14:47:54 +0100

    collegamento a mysql alla 1 versione del programma

--- peso.py
+++ peso.py
@@ -128,6 +128,37 @@ print (sheet['D9'].value)
 sheet = excel_document.get_sheet_by_name('Foglio1')
 print (sheet['E9'].value)
 
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="telemedicina2123"
+)
+
+print(mydb)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM post.dati_famigliari")
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+
+#inserisco nuovi record
+nome=input("inserisci nome: ")
+cognome=input("inserisci cogome: ")
+peso=input("inserisci peso: ")
+altezza=float(input("inserisci l'altezza: "))
+mycursor = mydb.cursor()
+sql = "INSERT INTO post.dati_famigliari(nome,cognome,peso,altezza)VALUES (%s,%s,%s,%s);" 
+val = (nome,cognome,peso,altezza)
+mycursor.execute(sql,val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
 
 
 #fine codice
