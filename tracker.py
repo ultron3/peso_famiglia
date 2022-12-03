6311aaa61c08ba9e4d84f4544ea926c61c9414e9
alex gnavi <alex.gnavi@gmail.com>
2022-12-03 10:59:52 +0100

    aggiunto commento linea 133

--- tracker.py
+++ tracker.py
@@ -130,6 +130,7 @@ print(df.to_string())
 
 #apro un documento excel utilizzando la libreria openpyxl
 #per capire che il file in excel è aperto il programma scrive class workbook.worbook
+#attenzione se si sbaglia a scrivere su una cella il programma non si rompe ma ti dice che c'è un errore in una cella speicifica
 import openpyxl
 excel_document = openpyxl.load_workbook('progressi_palestra.xlsx')
 print (type(excel_document))
