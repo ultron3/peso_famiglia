bd0366471f0ba73750fe8980b936c7f7a76a5477
alex gnavi <alex.gnavi@gmail.com>
2023-01-27 20:06:57 +0100

    come scaricare l'applicazione

---
+++ src/smart.js
@@ -0,0 +1,9 @@
+let msg = 'ciao';
+
+const pi=3.14; 
+
+if (msg === 'ciao'){
+    alert('Scarica da playstore o apple store MovitAIHealth ');
+}else{
+    alert('il messaggio non contiene ciao');
+}
