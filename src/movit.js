567553081b46a91b994af67a4f04d0887f78d3eb
alex gnavi <alex.gnavi@gmail.com>
2023-01-27 20:16:16 +0100

    aggiunto al file index un input

--- src/movit.js
+++ src/movit.js
@@ -3,7 +3,7 @@ let msg = 'ciao';
 const pi=3.14; 
 
 if (msg === 'ciao'){
-    alert('Ciao  sono  MovitAi il tuo assistente personale, prego inserisci i tuoi dati ');
+    alert('Ciao  sono  MovitAi il tuo assistente personale, prego inserisci i tuoi dati per registrarti ');
 }else{
     alert('il messaggio non contiene ciao');
 }
