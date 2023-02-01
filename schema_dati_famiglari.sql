055afa0dc5595632f9d2db79631f96d857fe846d
alex gnavi <alex.gnavi@gmail.com>
2023-02-01 16:58:38 +0100

    modifica schema sql e aggiunto file angulare/core

--- schema_dati_famiglari.sql
+++ schema_dati_famiglari.sql
@@ -1,3 +1,5 @@
 SELECT * FROM post.dati_famigliari;
 select nome,cognome from post.dati_famigliari;
 select peso,altezza from post.dati_famigliari;
+
+select altezza from post.dati_famigliari order BY nome;
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import{Browsermodule} from '@angular/core';
+
+@NgModule({
+
+imports:[
+    Browsermodule,
+
+]
+})
+export class AppModule {}
