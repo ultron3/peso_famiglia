11f4588c75a04bc9747dd16c85d4980fef26e7c0
alex gnavi <alex.gnavi@gmail.com>
2023-02-02 15:57:16 +0100

    new meassege

--- src/mongo.js
+++ src/mongo.js
@@ -1,3 +1,18 @@
+let msg = 'ciao';
+
+const pi=3.14; 
+
+if (msg === 'ciao'){
+    alert('collegamento a mongodb effettuato ');
+}else{
+    alert('il messaggio non contiene ciao');
+}
+
+
+
+
+
+
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
