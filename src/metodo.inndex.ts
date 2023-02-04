43c6d418dd024720d2ac4b073d2b55046ca731a0
alex gnavi <alex.gnavi@gmail.com>
2023-02-04 15:08:27 +0100

    modifica su file typescript

---
+++ src/metodo.inndex.ts
@@ -0,0 +1,7 @@
+import { Http2Server } from "http2";
+import { Http2Server } from "@angular/common/Http2Server";
+import {Observable} from "rxjs";
+
+
+//importo i pacchetti necessarri 
+//installare versione angular 8.0.0 attraverso il comando ng install -g @angular@8.0.0
