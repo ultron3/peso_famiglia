5ae5ef5edfd01f620a845bb341b8ada4af1dd4c5
alex gnavi <alex.gnavi@gmail.com>
2023-02-06 13:52:28 +0100

    install npm node

--- src/demo_http.js
+++ src/demo_http.js
@@ -1,3 +1,22 @@
+async function getData() {
+  return Promise.resolve('data');
+}
+
+async function getMoreData(data) {
+  return Promise.resolve(data + 'more data');
+}
+
+async function getAll() {
+  const data = await getData();
+  const moreData = await getMoreData(data);
+  return `All the data: ${data}, ${moreData}`;
+}
+
+getAll().then((all) => {
+  console.log('all the data')
+})
+
+
 var http = require('http');
 
 //create a server object:
