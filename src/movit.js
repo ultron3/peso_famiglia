b81449d847f61deeb9e1725fc8bbdef61b297a11
alex gnavi <alex.gnavi@gmail.com>
2023-02-08 16:21:33 +0100

    spostato tutto in mi app

--- src/movit.js
+++
@@ -1,9 +0,0 @@
-let msg = 'ciao';
-
-const pi=3.14; 
-
-if (msg === 'ciao'){
-    alert('Ciao  sono  MovitAi il tuo assistente personale, prego inserisci i tuoi dati per registrarti ');
-}else{
-    alert('il messaggio non contiene ciao');
-}
--- src/smart.js
+++
@@ -1,9 +0,0 @@
-let msg = 'ciao';
-
-const pi=3.14; 
-
-if (msg === 'ciao'){
-    alert('Scarica da playstore o apple store MovitAIHealth ');
-}else{
-    alert('il messaggio non contiene ciao');
-}
