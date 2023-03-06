c3bf2a34b2a966f8718054d2b63e8fa9939d2153
alex gnavi <alex.gnavi@gmail.com>
2023-03-06 09:52:52 +0100

    possibilità di creare un codice qr ma c'è un errore in riga 13

--- grafici src/index.js
+++ grafici src/index.js
@@ -1,8 +1,23 @@
 let docTitle = document.title;
-window.addEventListener("blur",()=>{
+window.addEventListener("blur", () => {
     document.title = "torna qui;(";
 });
 
-window.addEventListener("focus",()=>{
-return
+window.addEventListener("focus", () => {
+    return
 });
+
+function generateQR() {
+    document.querySelector("#qr-image").style.display = "block";
+    let Qrtext = document.querySelector("#text").ariaValueMax;
+    if (Qrtext.trim().length==0) {
+
+        document.querySelector("#qr-image .error").innerHTML = "enter text";
+        document.querySelector("#img").style.display = "none";
+    } else {
+        document.querySelector("#img").style.display = "block";
+        document.querySelector("#qr-img .error").innerHTML = "";
+        document.querySelector("#img").src = "https://api.qrserver.com/v1/create-qr-code/?size=240*2406data=" + Qrtext;
+    }
+
+}
