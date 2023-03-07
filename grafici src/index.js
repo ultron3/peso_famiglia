8f11ed6218cc2fcdaa40be12a894daa50786efe1
alex gnavi <alex.gnavi@gmail.com>
2023-03-07 12:01:51 +0100

    aggiunto menu a tendina

--- grafici src/index.js
+++ grafici src/index.js
@@ -9,6 +9,10 @@ window.addEventListener("focus", () => {
     return
 });
 
+
+
+
+
 function generateQR() {
     document.querySelector("#qr-image").style.display = "block";
     let Qrtext = document.querySelector("#text").value;
@@ -24,3 +28,23 @@ function generateQR() {
 
 }
 
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
